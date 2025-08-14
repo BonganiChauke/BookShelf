69f6aba668841f26c541603151375e3851dfedae
BonganiChauke <bonganichauke11@outlook.com>
2025-08-14 20:13:12 +0200

    Navbar to display at the end og the page

--- includes/header.php
+++ includes/header.php
@@ -26,7 +26,7 @@
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 justify-content-end flex-grow-1">
                         <li class="nav-item"><a class="nav-link" href="/index.php">Home</a></li>
                         <li class="nav-item"><a class="nav-link" href="/views/about.php">About US</a></li>
                         <li class="nav-item"><a class="nav-link" href="/views/contact.php">Contact Us</a></li>
