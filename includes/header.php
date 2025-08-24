f799849665604b5c3527156ca55c583806ff53b7
BonganiChauke <bonganichauke11@outlook.com>
2025-08-24 22:05:29 +0200

    Included the internal javascript file link

--- includes/header.php
+++ includes/header.php
@@ -16,6 +16,8 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
     <!-- style css -->
     <link rel="stylesheet" href="/assets/css/style.css">
+    <!-- script tag -->
+    <script src="../assets/javascript/script.js"></script>
 
 </head>
 
