b47342771cf1b04eaab4e6b7d1d59e04b9df6ecb
BonganiChauke <bonganichauke11@outlook.com>
2025-08-28 19:21:44 +0200

    Resolved merge conflicts

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 // DOMContentLoaded to load javascript when html is loaded to browser
-=======
-// DOMContentLoaded
->>>>>>> aa9a4e1995f0ba8671570feaf80a74ed6988dc2c
 document.addEventListener("DOMContentLoaded", (event) => {
 
     //contact form variables 
@@ -12,14 +8,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
     const message = document.getElementById("message");
     const phone = document.getElementById("phone_number");
 
-<<<<<<< HEAD
-    // select the form
-    const form = document.getElementById("save_message");
-=======
     // alert message errors
     const success_message = document.getElementById("success-message");
     const error_message = document.getElementById("error-message");
->>>>>>> aa9a4e1995f0ba8671570feaf80a74ed6988dc2c
 
     // error messages 
     // **********************************************************
@@ -47,7 +38,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
         input.style.border = '1px solid green';
     }
 
-<<<<<<< HEAD
     //reset input color
     function resetInputColor(input) {
         input.style.border = '1px solid black';
@@ -59,22 +49,15 @@ document.addEventListener("DOMContentLoaded", (event) => {
         document.getElementById("success-message").textContent = message;
         if (successAlert) {
             successAlert.classList.remove('d-none');
-=======
-    // show success alert
-    function showSuccessAlert() {
-
-        const successAlert = document.getElementById('alert-success');
-        
-        if (successAlert) {
-            successAlert.style.display = 'block';
->>>>>>> aa9a4e1995f0ba8671570feaf80a74ed6988dc2c
             setTimeout(() => {
                 successAlert.style.display = 'none';
             }, 5000);
-        }
-    }
 
-<<<<<<< HEAD
+        }
+
+    }
+    
+
     // show error alert dialog
     function showErrorAlert(message) {
         const errorAlert = document.getElementById('alert-danger');
@@ -85,7 +68,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
                 setTimeout(() => {
                     errorAlert.style.display = 'none';
                 }, 5000);
-=======
+            }
+        }
+    }
     // show error alert
     function showErrorAlert(message) {
         const errorAlert = document.getElementById('alert-danger');
@@ -98,12 +83,10 @@ document.addEventListener("DOMContentLoaded", (event) => {
                 setTimeout(() => {
                     errorAlert.style.display = 'none';
                 }, 3000);
->>>>>>> aa9a4e1995f0ba8671570feaf80a74ed6988dc2c
             }
         }
     }
 
-<<<<<<< HEAD
     //function to check all inputs if null
     function checkInputs() {
         const items = document.querySelectorAll('.item')
@@ -135,8 +118,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
             })
         }
     }
-=======
->>>>>>> aa9a4e1995f0ba8671570feaf80a74ed6988dc2c
 
     // ******************************************************
     // function to validate inputs
@@ -239,10 +220,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
             inputError(message);
         }
     });
-<<<<<<< HEAD
     // *******************************************************
-=======
->>>>>>> aa9a4e1995f0ba8671570feaf80a74ed6988dc2c
 
     // save message button event 
     document.getElementById("save_message")?.addEventListener("submit", (event) => {
@@ -250,7 +228,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
         // prevent default 
         event.preventDefault();
 
-<<<<<<< HEAD
         // //if to check all inputs fields values are valid
         // if (validateNames(first_name.value) || validateNames(last_name.value) || validateEmail(email.value) || validatePhoneNumber(phone.value)) {
 
@@ -294,13 +271,11 @@ document.addEventListener("DOMContentLoaded", (event) => {
             .catch(err => {
                 showErrorAlert("Request failed: " + err);
             });
-=======
         //if to check all inputs fields values are valid
         if (!validateNames(first_name.value)) {
             alert('Yes');
             showSuccessAlert();
         }
->>>>>>> aa9a4e1995f0ba8671570feaf80a74ed6988dc2c
 
     });
 });
