e09a7faf3ef17b461b37d96f51f40b7bc8f295ad
BonganiChauke <bonganichauke11@outlook.com>
2025-08-29 20:52:51 +0200

    proccesing user inputs and sending to database

---
+++ process/process_contact.php
@@ -0,0 +1,38 @@
+<?php
+// including database connection 
+include '../includes/db_connect.php';
+
+// Only run if form was submitted
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // 1. Collect input values safely
+    $name = trim($_POST['name']);
+    $last_name = trim($_POST['last_name']);
+    $email = trim($_POST['email']);
+    $phone = trim($_POST['phone']);
+    $message = trim($_POST['message']);
+    $createdAt = date('Y-m-d H:i:s');
+
+    // prepare the SQL statement with placeholders question marks to prevent sql injections
+    $sql = "INSERT INTO contact_messages (name, last_name, email, phone, message, created_at) 
+            VALUES (?, ?, ?, ?, ?, ?)";
+
+    $stmt = $conn->prepare($sql);
+
+    if ($stmt === false) {
+        die("Prepare failed: " . $conn->error);
+    }
+
+    // bind parameters using s as string
+    $stmt->bind_param("ssssss", $name, $last_name, $email, $phone, $message, $createdAt);
+
+    // execute query and check result
+    if ($stmt->execute()) {
+        echo "Message saved successfully!";
+    } else {
+        echo "Insert failed: " . $stmt->error;
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+?>
