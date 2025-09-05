8f30e9a5d7113b14913c8836bd80de8786934709
BonganiChauke <bonganichauke11@outlook.com>
2025-09-05 22:16:58 +0200

    Footer content added

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,159 @@
 </main>
 
-<footer>
-    <div class="container">
-        <p class="text-center">© 2025 Bookshelf. All rights reserved.</p>
+
+<!-- footer -->
+<footer class="pt-3 pt-md-4 pt-xl-5">
+    <div>
+        <div class="container">
+            <div class="row gy-3 align-items-center">
+                <div class="col-12 col-sm-5 col-md-4 col-lg-3">
+                    <div class="footer-logo-wrapper text-start">
+                        <a class="display-6 fw-bold" href="#!">
+                            BookShelf
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+    
+    <section class="py-4 py-md-5 py-xl-6">
+        <div class="container">
+            <div class="row gy-3">
+                <div class="col-12 col-lg-7 col-xl-6">
+                    <div class="widget">
+                        <h4 class="widget-title mb-3">Subscribe to Our Newsletter</h4>
+                        <p class="mb-4">
+                            Don't miss out on the opportunity to be part of something great. <br>
+                            Subscribe now and let the journey begin!
+                        </p>
+                        <form id="subscribe_form" method="post" action="#!">
+                            <div class="row gy-5">
+                                <div class="col-12 col-md-8">
+                                    <!-- email input -->
+                                        <div class="form-floating">
+                                            <input type="email" class="item form-control" name="email" id="email"
+                                                placeholder="">
+                                            <label for="email">Email</label>
+                                            <span class="error-msg text-danger"></span>
+                                        </div>
+                                </div>
+                                <div class="col-12 col-lg-8">
+                                    <div class="d-grid mb-3 mb-lg-0">
+                                        <button class="btn btn-primary" type="submit">Subscribe</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-2 col-xl-2">
+                    <div class="link-wrapper">
+                        <ul class="m-0 list-unstyled">
+                            <li class="mb-3">
+                                <a href="#!" class="link-dark text-decoration-none link-opacity-75-hover">
+                                    Our Values
+                                </a>
+                            </li>
+                            <li class="mb-3">
+                                <a href="#!" class="link-dark text-decoration-none link-opacity-75-hover">
+                                    Beliefs
+                                </a>
+                            </li>
+                            <li class="mb-3">
+                                <a href="#!" class="link-dark text-decoration-none link-opacity-75-hover">
+                                    Educational Impact
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-3 col-xl-4">
+                    <div class="link-wrapper">
+                        <ul class="m-0 list-unstyled">
+                            <li class="mb-3">
+                                <a href="#!" class="link-dark text-decoration-none link-opacity-75-hover">
+                                    Shipping & Returns
+                                </a>
+                            </li>
+                            <li class="mb-3">
+                                <a href="#!" class="link-dark text-decoration-none link-opacity-75-hover">
+                                    Refund Policy
+                                </a>
+                            </li>
+                            <li class="mb-3">
+                                <a href="#!" class="link-dark text-decoration-none link-opacity-75-hover">
+                                    Terms and Conditions
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#!" class="link-dark text-decoration-none link-opacity-75-hover">
+                                    Privacy
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    
+    <div class="py-3 py-md-4 py-xl-5 bg-light">
+        <div class="container">
+            <div class="row g-3 align-items-center">
+                <div class="col-12 col-lg-6 order-1 order-lg-0">
+                    <div class="copyright-wrapper d-block mb-1 fs-8 text-center text-lg-start">
+                        © 2025. All Rights Reserved.
+                    </div>
+                    <div class="credit-wrapper d-block text-secondary fs-8 text-center text-lg-start">
+                        Built by<a href="https://bonganichauke.vercel.app/" class="link-secondary text-decoration-none">
+                            Bongani Chauke</a>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6 order-0 order-lg-1">
+                    <div class="link-wrapper">
+                        <ul
+                            class="m-0 list-unstyled d-flex justify-content-center justify-content-lg-end gap-2 gap-md-3">
+                            <li>
+                                <a href="#!"
+                                    class="link-underline-opacity-0 link-opacity-75-hover link-underline-opacity-100-hover link-offset-1 link-secondary fs-8 d-flex align-items-center pe-2 pe-md-3 bsb-sep bsb-sep-border">
+                                    Shipping
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#!"
+                                    class="link-underline-opacity-0 link-opacity-75-hover link-underline-opacity-100-hover link-offset-1 link-secondary fs-8 d-flex align-items-center pe-2 pe-md-3 bsb-sep bsb-sep-border">
+                                    Refund
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#!"
+                                    class="link-underline-opacity-0 link-opacity-75-hover link-underline-opacity-100-hover link-offset-1 link-secondary fs-8 d-flex align-items-center pe-2 pe-md-3 bsb-sep bsb-sep-border">
+                                    Warranty
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#!"
+                                    class="link-underline-opacity-0 link-opacity-75-hover link-underline-opacity-100-hover link-offset-1 link-secondary fs-8 d-flex align-items-center pe-2 pe-md-3 bsb-sep bsb-sep-border">
+                                    Privacy
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#!"
+                                    class="">
+                                    <i class="fa fa-facebook-square" aria-hidden="true"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </footer>
+
 
 </body>
 
