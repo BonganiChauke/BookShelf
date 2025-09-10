3a2e4e1d1740d0e1a475bd975975faf0ea49553e
BonganiChauke <bonganichauke11@outlook.com>
2025-09-10 20:32:22 +0200

    Insert statement to database

---
+++ controllers/register_controller.php
@@ -0,0 +1,37 @@
+<?php
+// including database connection 
+include '../includes/db_connect.php';
+
+// if to check if the form has been submitted
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    //collect user inputs
+    $first_name = trim($_POST['name']);
+    $last_name = trim($_POST['last_name']);
+    $email = trim($_POST['email']);
+    $password = password_hash(trim($_POST['password']), PASSWORD_DEFAULT);
+    $role = "customer";
+    $createdAt = date('Y-m-d H:i:s');
+
+    // sql insert statement
+    $sql = "INSERT INTO users (name, last_name, email, password, role, created_at) VALUES (?,?,?,?,?,?)";
+
+    // prepare sql query
+    $stmt = $conn->prepare($sql);
+
+    // bind parameters
+    $stmt -> bind_param("ssssss", $name, $last_name, $email, $password, $role, $createdAt);
+
+    // execute query and check result
+    if ($stmt->execute()) {
+        echo "Message saved successfully!";
+    } else {
+        echo "Insert failed: " . $stmt->error;
+    }
+
+    $stmt->close();
+    $conn->close();
+
+}
+
+?>
