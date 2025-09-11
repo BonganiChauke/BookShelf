8dd2fce2c0fc5f986d98275752c10fd2617c8948
BonganiChauke <bonganichauke11@outlook.com>
2025-09-11 21:52:39 +0200

    Changed ID names for each input field

--- register.php
+++ register.php
@@ -22,8 +22,8 @@
 
                         <div class="card-body py-5 px-md-5">
 
-                            <!-- register form -->
-                            <form id="register_form" action="" method="post">
+                            <!-- register form  action="./controllers/register_controller.php" -->
+                            <form id="register_form"  method="post">
 
                                 <!-- error message alert -->
                                 <div class="alert alert-danger alert-dismissible fade show d-none" id="alert-danger"
@@ -42,7 +42,7 @@
                                     <div class="col-md-6 mb-4">
                                         <!-- first name input -->
                                         <div class="form-floating mb-2">
-                                            <input type="text" id="first_name" name="name" class="item form-control"
+                                            <input type="text" id="reg_first_name" name="name" class="item form-control"
                                                 placeholder="" />
                                             <label class="form-label" for="first_name">First Name</label>
                                             <span class="error-msg text-danger"></span>
@@ -52,7 +52,7 @@
                                     <div class="col-md-6 mb-4">
                                         <!-- last name input -->
                                         <div class="form-floating mb-2">
-                                            <input type="text" id="last_name" name="last_name" class="item form-control"
+                                            <input type="text" id="reg_last_name" name="last_name" class="item form-control"
                                                 placeholder="" />
                                             <label class="form-label" for="first_name">Last Name</label>
                                             <span class="error-msg text-danger"></span>
@@ -63,7 +63,7 @@
 
                                 <!-- email input -->
                                 <div class="form-floating mb-4">
-                                    <input type="email" class="item form-control" name="email" id="email"
+                                    <input type="email" class="item form-control" name="email" id="reg_email"
                                         placeholder="">
                                     <label for="email">Email</label>
                                     <span class="error-msg text-danger"></span>
@@ -77,7 +77,7 @@
                                         <div class="input-group">
                                             <div class="form-floating flex-grow-1">
                                                 <input type="password" class="item form-control border-end-0"
-                                                    id="password" name="password" placeholder="">
+                                                    id="reg_password" name="password" placeholder="">
                                                 <label for="password">Password</label>
                                             </div>
                                             <div class="input-group-text border-start-0 bg-transparent">
@@ -95,7 +95,7 @@
                                         <div class="input-group">
                                             <div class="form-floating flex-grow-1">
                                                 <input type="password" class="item form-control border-end-0"
-                                                    id="confirm_password" name="confirm_password" placeholder="">
+                                                    id="reg_confirm_password" name="confirm_password" placeholder="">
                                                 <label for="confirm_password">Confirm Password</label>
                                             </div>
                                             <div class="input-group-text border-start-0 bg-transparent">
