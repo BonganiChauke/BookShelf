9153367d6c761cc07c7eb5175abf16515e40c8d8
BonganiChauke <bonganichauke11@outlook.com>
2025-09-15 20:50:07 +0200

    A file will contain input validation functions

---
+++ Validation/validation.php
@@ -0,0 +1,25 @@
+<?php
+
+// this class will have functions to handle input validation
+class validation
+{
+    // error input messages
+    public $first_name_error = $last_name_error = $email_error = $password_error == "";
+
+    // first name function validation
+    public static function namesValidation($firstname): string
+    {
+        if (empty($first_name)) {
+            $first_name_error = "First Name Required";
+        } else if (strlen($first_name) < 3) {
+            $first_name_error = "First Name must be at least 3 characters";
+        }else{
+            $first_name_error = "";
+        }
+        return $first_name_error;
+    }
+
+
+}
+
+?>
