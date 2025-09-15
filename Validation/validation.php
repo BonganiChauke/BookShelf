48e5d581267362cd4122e7de063c79fc69fc748e
BonganiChauke <bonganichauke11@outlook.com>
2025-09-15 21:24:49 +0200

    Add username parameter to function to use it for either first or last name validation and updated the first_name_error to name

--- Validation/validation.php
+++ Validation/validation.php
@@ -6,13 +6,30 @@ class validation
     // error input messages
     public static $name_error = $last_name_error = $email_error = $password_error == "";
 
-    // first name function validation
-    public static function namesValidation($firstname): string
+    // names function validation
+    public static function namesValidation($name, $username): string
     {
-        if (empty($first_name)) {
-            $first_name_error = "First Name Required";
-        } else if (strlen($first_name) < 3) {
-            $first_name_error = "First Name must be at least 3 characters";
+        if (empty($name)) {
+            $name_error = $username. " Name Required";
+        } else if (strlen($name) < 3) {
+            $name_error = $username. " must be at least 3 characters";
+        }else{
+            $name_error = "";
+        }
+        return $name_error;
+    }
+
+    //email validation function
+    public static function emailValidation($email): string {
+        
+        // verify if the domain part of the email address has valid Mail Exchange (MX) records
+        $domain = substr($email, strpos($email, '@') + 1);
+
+        if(empty($email)){
+            $email_error  = "Email Required";
+        }
+        else if (filter_var($email, FILTER_VALIDATE_EMAIL) && checkdnsrr($domain, 'MX')){
+            $email_error = "Email Invalid";
         }else{
             $first_name_error = "";
         }
